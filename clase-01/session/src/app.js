2fd9f109002accaa940d5d79eefdaa7ec82137b7
Luis Mera <profeluismeradev@gmail.com>
2024-10-23 20:46:12 -0300

    clase 1

---
+++ clase-01/session/src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import handlebars from "express-handlebars";
+import _dirname from "./dirname.js";
+import viewRoutes from "./routes/views.routes.js";
+
+const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", _dirname + "/views");
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
+
+app.use("/", viewRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`);
+});
