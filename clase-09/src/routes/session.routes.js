e89a419417bc28df3359cd9cc1a3b370ac277b6a
Luis Mera <luismera86@gmail.com>
2024-11-27 20:28:37 -0300

    clase 9

--- clase-09/src/routes/session.routes.js
+++ clase-09/src/routes/session.routes.js
@@ -6,42 +6,16 @@ import passport from "passport";
 import { createToken, verifyToken } from "../utils/jwt.js";
 import { passportCall } from "../middlewares/passport.middleware.js";
 import { authorization } from "../middlewares/authorization.middleware.js";
+import { SessionController } from "../controllers/session.controller.js";
 
+const sessionController = new SessionController();
 const router = Router();
 
-router.post("/register", passportCall("register"), async (req, res) => {
-  try {
-    res.status(201).json({ status: "success", msg: "Usuario Registrado" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: "Error", msg: "Error interno del servidor" });
-  }
-});
+router.post("/register", passportCall("register"), sessionController.register);
 
-router.post("/login", passportCall("login"), async (req, res) => {
-  try {
-      // Generamos el token
-      const token = createToken(req.user);
+router.post("/login", passportCall("login"), sessionController.login);
 
-      // Guardamos el token en una cookie
-      res.cookie("token", token, { httpOnly: true });
-
-    res.status(200).json({ status: "success", payload: req.user });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: "Error", msg: "Error interno del servidor" });
-  }
-});
-
-router.get("/logout", async (req, res) => {
-  try {
-    req.session.destroy();
-    res.status(200).json({ status: "success", msg: "Session cerrada" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: "Error", msg: "Error interno del servidor" });
-  }
-});
+router.get("/logout", sessionController.logout);
 
 router.get("/current", passportCall("jwt"), authorization("user"), async (req, res) => {
   try {
@@ -63,7 +37,5 @@ router.get(
     res.status(200).json({ status: "success", payload: req.user });
   }
 );
-
-
 
 export default router;
